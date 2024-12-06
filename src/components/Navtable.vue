f14ef990bd531aa0e6d5e8334f5b5fb29fde6417
Eric <ericchu910306@gmail.com>
2024-12-06 20:01:23 +0800

    製作 navtable

--- src/components/Navtable.vue
+++ src/components/Navtable.vue
@@ -1,51 +1,97 @@
 <template>
-    <nav class="header">
-        <ul class="menu">
-        <li>
-          <a href="index.asp">首頁</a>
-        </li>
-        <li>
-          <a href="javascript:void();" class="had_sub_menu cur_hand">計畫介紹</a>
-        <ul class="menu-hover">
-        <li>
-          <a href="plangoal.asp">計畫目標</a>
-        </li>
-        <li>
-          <a href="teacher.asp">師資培育</a>
-        </li>
-        <li>
-          <a href="student.asp">學生培訓</a>
-        </li>
-        </ul>
-        </li>
-        <li>
-          <a href="signup_main.asp">線上報名</a>
-        </li>
-        <li>
-          <a href="class_main.asp">線上課程</a>
-        </li>
-        <li>
-          <a href="test_main.asp">線上測驗</a>
-        </li>
-        <li>
-          <a href="download_main.asp">下載專區</a>
-        </li>
-        <li>
-          <a href="teamanage.asp">教師管理</a>
-        </li>
-        </ul>
-    </nav>
+  <v-card class="custom-tabs">
+    <v-row class="d-flex justify-center">
+      <v-col cols="auto">
+        <v-tabs v-model="tab">
+          <v-tab value="首頁">
+            <v-btn class="btn">首頁</v-btn>
+          </v-tab>
+          <v-tab>
+              <v-menu class="custom-menu">
+                <template  v-slot:activator="{ props: menu }">
+                  <v-tooltip location="top" > 
+                    <template  v-slot:activator="{ props: tooltip }" >
+                      <v-btn class="btn" v-bind="mergeProps(menu, tooltip)">
+                        計畫介紹
+                      </v-btn>
+                    </template>
+                  </v-tooltip>
+                </template>
+                <v-list >
+                  <v-list-item v-for="(item, index) in items" :key="index" class="v-list-item">
+                    <v-list-item-title >
+                      <v-btn @click="onButtonClick(item.title)" class="btn" outlined>
+                        {{ item.title }}
+                      </v-btn>
+                    </v-list-item-title>
+                  </v-list-item>
+                </v-list>
+              </v-menu>
+          </v-tab>
+          <v-tab >
+            <v-btn class="btn">線上報名</v-btn>
+          </v-tab>
+          <v-tab >
+            <v-btn class="btn">線上課程</v-btn>
+          </v-tab>
+          <v-tab >
+            <v-btn class="btn">線上測驗</v-btn>
+          </v-tab>
+          <v-tab >
+            <v-btn class="btn">下載專區</v-btn>
+          </v-tab>
+        </v-tabs>
+      </v-col>
+    </v-row>
+
+  </v-card>
 </template>
-<style scoped>
-.header {
-  color: white; /* 文字顏色 */
-  padding: 1rem 0; /* 上下內邊距 */
-  display: flex; /* 使用 Flexbox */
-  justify-content: center; /* 水平居中 */
-  align-items: center; /* 垂直居中 */
-  width: 100%; /* 滿版寬度 */
-  box-sizing: border-box; /* 包含內邊距 */
+
+<script>
+import { mergeProps } from 'vue';
+
+export default {
+  data: () => ({
+    tab: null,
+    items: [
+      { title: '計畫目標', route: '/page1'},  //跳轉計畫目標頁面
+      { title: '師資培育' , route: '/page1'}, //跳轉師資培育頁面
+      { title: '學生培育' , route: '/page1'}, //跳轉學生培育頁面
+    ],
+  }),
+  methods: {
+    mergeProps,
+    onButtonClick(title) {
+      // 當按鈕被點擊時執行的動作
+      console.log(`You clicked: ${title}`);
+    },
+  },
 }
+</script>
+
+<style scoped>
+.custom-tabs {
+  font-size: 20px;
+  background-color: #2f87cf;
+}
+
+.btn {
+  font-size: 25px;
+  box-shadow: none;
+  background-color:  #2f87cf;
+  font-family: '微軟黑體', sans-serif;
+}
+
+.v-list-item {
+  background-color:  #2f87cf;
+}
+
+.custom-menu .v-menu__content {
+  border: none; /* 去除邊框 */
+  background-color: #31d9e4; /* 改變背景顏色 */
+}
+
 </style>
+
 
 
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,10 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Header from '@/components/Header.vue';
 import Footer from '@/components/Footer.vue';
 
+// import Page1 from '../views/Page1.vue';  //計畫目標
+// import Page2 from '../views/Page2.vue';  //師資培育
+// import Page3 from '../views/Page3.vue';  //學生培育
+
 
 const routes = [
     {
@@ -15,8 +19,12 @@ const routes = [
       component:Footer,
       
     },
+     
+    // { path: '/page1', component: Page1 },
+    // { path: '/page2', component: Page2 },
+    // { path: '/page3', component: Page3 },
   ];
-  
+
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
